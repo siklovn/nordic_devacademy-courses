1c706332642913181b437407baa6d31980175fdb
siklovn <siklovning@gmail.com>
2025-09-24 13:12:39 +0200

    Complete l2_e2

--- nRF_Connect_SDK_Fundamentals/l2/l2_e2/src/main.c
+++ nRF_Connect_SDK_Fundamentals/l2/l2_e2/src/main.c
@@ -10,7 +10,7 @@
 #include <zephyr/drivers/gpio.h>
 
 /* STEP 9 - Increase the sleep time from 100ms to 10 minutes  */
-#define SLEEP_TIME_MS 100
+#define SLEEP_TIME_MS 10*60*1000
 
 /* SW0_NODE is the devicetree node identifier for the "sw0" alias */
 #define SW0_NODE DT_ALIAS(sw0)
@@ -21,8 +21,13 @@ static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 /* STEP 4 - Define the callback function */
+void button_pressed (const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+{
+	gpio_pin_toggle_dt(&led);
+}
 
 /* STEP 5 - Define a variable of type static struct gpio_callback */
+static struct gpio_callback button_cb_data;
 
 int main(void)
 {
@@ -46,15 +51,16 @@ int main(void)
 		return -1;
 	}
 	/* STEP 3 - Configure the interrupt on the button's pin */
+	ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
 
 	/* STEP 6 - Initialize the static struct gpio_callback variable   */
+	gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
 
 	/* STEP 7 - Add the callback function by calling gpio_add_callback()   */
+	gpio_add_callback(button.port, &button_cb_data);
 
 	while (1) {
 		/* STEP 8 - Remove the polling code */
-		bool val = gpio_pin_get_dt(&button);
-		gpio_pin_set_dt(&led, val);
 
 		k_msleep(SLEEP_TIME_MS);
 	}

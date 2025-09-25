faf36b392fe1ebc2db737a2166e53bac74a9325b
siklovn <siklovning@gmail.com>
2025-09-25 13:31:21 +0200

    Complete l7_e1

--- nRF_Connect_SDK_Fundamentals/l7/l7_e1/src/main.c
+++ nRF_Connect_SDK_Fundamentals/l7/l7_e1/src/main.c
@@ -8,11 +8,17 @@
 #include <zephyr/sys/printk.h>
 
 /* STEP 2 - Define stack size and scheduling priority used by each thread */
+#define STACKSIZE 1024
+#define THREAD0_PRIORITY 7
+#define THREAD1_PRIORITY 7
 
 void thread0(void)
 {
 	while (1) {
 		/* STEP 3 - Call printk() to display a simple string "Hello, I am thread0" */
+		printk("Hello, I am thread0\n");
+		k_msleep(5);
+
 		/* STEP 6 - Make the thread yield */
 		/* STEP 10 - Put the thread to sleep */
 		/* Remember to comment out the line from STEP 6 */
@@ -23,6 +29,9 @@ void thread1(void)
 {
 	while (1) {
 		/* STEP 3 - Call printk() to display a simple string "Hello, I am thread1" */
+		printk("Hello, I am thread1\n");
+		k_msleep(5);
+
 		/* STEP 8 - Make the thread yield */
 		/* STEP 10 - Put the thread to sleep */
 		/* Remember to comment out the line from STEP 8 */
@@ -30,3 +39,5 @@ void thread1(void)
 }
 
 /* STEP 4 - Define and initialize the two threads */
+K_THREAD_DEFINE(thread0_id, STACKSIZE, thread0, NULL, NULL, NULL, THREAD0_PRIORITY, 0, 0);
+K_THREAD_DEFINE(thread1_id, STACKSIZE, thread1, NULL, NULL, NULL, THREAD1_PRIORITY, 0, 0);

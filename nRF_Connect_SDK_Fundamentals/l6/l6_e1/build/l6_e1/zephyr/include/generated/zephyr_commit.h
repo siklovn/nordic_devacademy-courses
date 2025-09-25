baea07f4bcf0165eb6b43b3a9765204ed59c632d
siklovn <siklovning@gmail.com>
2025-09-25 12:04:13 +0200

    Completed l6_e1 (no sensor board tested)

---
+++ nRF_Connect_SDK_Fundamentals/l6/l6_e1/build/l6_e1/zephyr/include/generated/zephyr_commit.h
@@ -0,0 +1,12 @@
+#ifndef _ZEPHYR_COMMIT_H_
+#define _ZEPHYR_COMMIT_H_
+
+/*  values come from cmake/version.cmake
+ * BUILD_COMMIT related  values will be 'git rev-parse',
+ * alternatively user defined BUILD_VERSION.
+ */
+
+#define ZEPHYR_COMMIT                   1612683d4010
+#define ZEPHYR_COMMIT_STRING            "1612683d4010"
+
+#endif /* _ZEPHYR_COMMIT_H_ */

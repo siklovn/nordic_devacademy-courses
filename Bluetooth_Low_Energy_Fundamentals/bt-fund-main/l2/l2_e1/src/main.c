56a27a5c1837bc6b10fa658edb5f7f6cee289240
siklovn <siklovning@gmail.com>
2025-09-26 11:57:38 +0200

    Complete l2_e1

--- Bluetooth_Low_Energy_Fundamentals/bt-fund-main/l2/l2_e1/src/main.c
+++ Bluetooth_Low_Energy_Fundamentals/bt-fund-main/l2/l2_e1/src/main.c
@@ -6,7 +6,10 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+
 /* STEP 3 - Include the header file of the Bluetooth LE stack */
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/gap.h>
 
 #include <dk_buttons_and_leds.h>
 
@@ -19,10 +22,25 @@ LOG_MODULE_REGISTER(Lesson2_Exercise1, LOG_LEVEL_INF);
 #define RUN_LED_BLINK_INTERVAL 1000
 
 /* STEP 4.1.1 - Declare the advertising packet */
+static const struct bt_data ad[] = {
+	/* STEP 4.1.2 - Set the advertising flags */
+	BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_NO_BREDR),
+
+	/* STEP 4.1.3 - Set the advertising packet data */
+	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
+};
 
 /* STEP 4.2.2 - Declare the URL data to include in the scan response */
+static unsigned char url_data[] ={0x17,'/','/','a','c','a','d','e','m','y','.',
+                                 'n','o','r','d','i','c','s','e','m','i','.',
+                                 'c','o','m'};
 
 /* STEP 4.2.1 - Declare the scan response packet */
+static const struct bt_data sd[] = {
+	/* 4.2.3 Include the URL data in the scan response packet */
+	BT_DATA(BT_DATA_URI, url_data, sizeof(url_data))
+
+};
 
 int main(void)
 {
@@ -36,9 +54,21 @@ int main(void)
 		LOG_ERR("LEDs init failed (err %d)\n", err);
 		return -1;
 	}
+
 	/* STEP 5 - Enable the Bluetooth LE stack */
+	err = bt_enable(NULL);
+	if (err) {
+		LOG_ERR("Bluetooth init failed (err %d)\n", err);
+		return -1;
+	}
+	LOG_INF("Bluetooth initialized\n");
 
 	/* STEP 6 - Start advertising */
+	err = bt_le_adv_start(BT_LE_ADV_NCONN, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
+	if (err) {
+		LOG_ERR("Advertising failed to start (err %d)\n", err);
+		return -1;
+	}
 
 	LOG_INF("Advertising successfully started\n");
 
